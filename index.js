b6b3b5f00fdb9bf79a45643b19cfe351912364f8
Ahmed Missouri <ahmedmissouri@icloud.com>
2022-11-14 12:24:29 +0000

    Simple FizzBuzz solution

---
+++ index.js
@@ -0,0 +1,12 @@
+const args = process.argv
+const number = args[2]
+
+if (number % 3 === 0 && number % 5 != 0) {
+    console.log('Fizz')
+} else if (number % 5 === 0 && number % 3 != 0) {
+    console.log('Buzz')
+} else if (number % 3 === 0 && number % 5 === 0) {
+    console.log('FizzBuzz')
+} else {
+    console.log(number)
+}
