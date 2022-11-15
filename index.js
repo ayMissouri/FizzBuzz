67086a8aedbb9ca6e38c83da12ebcee39c67e2f8
Ahmed Missouri <ahmedmissouri@icloud.com>
2022-11-15 13:35:38 +0000

    Added new method

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ if (number % 3 === 0 && number % 5 != 0) {
 } else {
     console.log(number)
 }
+
+
+
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 != 0) {
+        console.log('Fizz')
+    } else if (i % 5 === 0 && i % 3 != 0) {
+     console.log('Buzz')
+    } else if (i % 3 === 0 && i % 5 === 0) {
+     console.log('FizzBuzz')
+    } else {
+     console.log(i)
+    }
+}
